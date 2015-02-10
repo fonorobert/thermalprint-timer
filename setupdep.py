149f1a0b3e38f7ed5a311d9f7f07bf0a3607cf79
fonorobert <fonorobert@gmail.com>
2015-02-10 21:53:49 +0100

    fix call syntax

--- setupdep.py
+++ setupdep.py
@@ -2,7 +2,7 @@
 
 from subprocess import call
 
-call('git submodule update')
+call(['git', 'submodule update'])
 call(['virtualenv venv', '-p python3.4'])
-call('venv/bin/pip install Pillow')
+call(['venv/bin/pip', 'install Pillow'])
 call(['pyusb/setup.py', 'install'])
