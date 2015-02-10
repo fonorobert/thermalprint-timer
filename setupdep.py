a21234a61d42f19a5f5885fff90d5017d5df671c
fonorobert <fonorobert@gmail.com>
2015-02-10 21:52:03 +0100

    first version of setup script

---
+++ setupdep.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from subprocess import call
+
+call('git submodule update')
+call(['virtualenv venv', '-p python3.4'])
+call('venv/bin/pip install Pillow')
+call(['pyusb/setup.py', 'install'])
