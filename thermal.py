b873637f17866a9fbbb363ac264989f45432db28
fonorobert <fonorobert@gmail.com>
2015-02-11 22:23:35 +0100

    build header, footer and body functions

--- thermal.py
+++ thermal.py
@@ -1,100 +1,131 @@
-#!/usr/bin/python
-#venv/bin/python3
+#!venv/bin/python
+
 import sys
 sys.path.append('python-escpos')
+import argparse
+from escpos import *
+from utility import linebreak
 
-from escposmod import *
-
-import configparser
-
-config = configparser.ConfigParser()
-config.read('config.txt')
+parser = argparse.ArgumentParser()
 
 
-string = config['test']['string']
+# config = configparser.ConfigParser()
+# config.read('config.txt')
+
+
+# string = config['test']['string']
 
 # chars = config['i18n']['chartable']
 # noacc = config['i18n']['noaccents']
 # hexac = config['i18n']['hexaccent']
 
-# list = linebreak(string)
-# lines = []
-# for chunk in list:
-#     lines.append(i18n(chunk, chars, charr))
+
+
 
 # # for line in lines:
 # #     print line
 # # exit(0)
 
 
-# Epson = printer.Usb(0x04b8, 0x0e15)
-# Epson.set(align="center", font="A", char='int')
-# Epson.control('LF')
-# Epson.control('LF')
-
-# for line in lines:
-#     Epson.text(line)
-#     Epson.control('LF')
-# Epson.control('LF')
-# Epson.control('LF')
-# Epson.control('LF')
-# Epson.control('LF')
-# Epson.control('LF')
-# Epson.control('LF')
-
-# Epson.cut()
+Epson = printer.Usb(0x04b8, 0x0e15)
+Epson.set(align="center", font="A")
 
 
-class Printer:
-    def __init__(self):
-        config = configparser.ConfigParser()
-        config.read('config.txt')
-        self.chars = config['i18n']['chartable']
-        self.noacc = config['i18n']['noaccents']
-        self.hexac = config['i18n']['hexaccent']
-        self.imgdef = config['defaults']['imgpath']
-        self.Epson = printer.Usb(0x04b8, 0x0e15)
-        self.Epson.set(align="center", font="A", char='int')
+def _print_text(string, width=48):
+    printable = linebreak(string, width)
+    if isinstance(printable, str):
+        Epson.text(printable)
+    else:
+        for line in printable:
+            Epson.text(line)
+            Epson.control('LF')
 
-    def linebreak(self, string, charlimit=48):
-        if len(string) < charlimit:
-            return string
-        else:
-            words = string.split(' ')
-            chunks = ['']
-            for word in words:
-                if chunks == [] or len(chunks[-1])+len(word) > charlimit:
-                    chunks.append(word + ' ')
-                else:
-                    chunks[-1] += word + ' '
-            return chunks
 
-    def i18n(self, string, chars, charcodes):
-        for char in chars:
-            string = string.replace(char, charcodes[chars.index(char)])
-        return string
+def _print_img(imgpath):
+    Epson.image(imgpath)
+    Epson.control('LF')
 
-    def print_text(self, text, accent=False):
-        list = self.linebreak(text)
-        lines = []
-        if accent is True:
-            for chunk in list:
-                lines.append(self.i18n(chunk, self.chars, self.hexac))
-        elif accent is False:
-            for chunk in list:
-                lines.append(self.i18n(chunk, self.chars, self.noacc))
 
-        for line in lines:
-            self.Epson.text(line)
-            self.Epson.control('LF')
+def print_header(string, imgpath=None, string2=False, border="=*=|", width=48):
+    Epson.text(border*(width/len(border)))
+    Epson.control('LF')
 
-    def print_img(self, imgpath='default'):
-        if imgpath is 'default':
-            imgpath = self.imgdef
-        self.Epson.image(imgpath)
-        self.Epson.control('LF')
+    _print_img(imgpath)
 
-printer = Printer()
-printer.print_text(string)
-printer.print_img(imgpath='img/logo.jpg')
-#printer.print_img()
+    _print_text(string)
+
+    if isinstance(string2, str):
+        Epson.control('LF')
+        Epson.control('LF')
+        _print_text(string2)
+    elif isinstance(string2, list):
+        Epson.control('LF')
+        Epson.control('LF')
+        for line in string2:
+            _print_text(line)
+            Epson.control('LF')
+
+    Epson.control('LF')
+    Epson.text(border*(width/len(border)))
+    Epson.control('LF')
+    Epson.control('LF')
+    Epson.control('LF')
+    Epson.control('LF')
+
+
+def print_body(string):
+    Epson.set(align="left", font="A", width=2, height=2)
+    _print_text(string, width=48)
+    Epson.set(align="center", font="A")
+
+
+def print_footer(string, string2=False, border="=", width=48):
+    Epson.control('LF')
+    Epson.control('LF')
+
+    Epson.text(border*(width/len(border)))
+    Epson.control('LF')
+
+    _print_text(string)
+
+    if isinstance(string2, str):
+        Epson.control('LF')
+        Epson.control('LF')
+        _print_text(string2)
+    elif isinstance(string2, list):
+        Epson.control('LF')
+        Epson.control('LF')
+        for line in string2:
+            _print_text(line)
+            Epson.control('LF')
+
+    Epson.control('LF')
+    Epson.text(border*(width/len(border)))
+    Epson.control('LF')
+    Epson.control('LF')
+    Epson.control('LF')
+
+
+#======================================
+#Testing calls
+#======================================
+
+string4 = "Fertozobb kobanyak tangoharmonikak szennyezodes mutok apanazs vakulasig gyilkossag trombitamuveszek bracsak egymilliard kocsagok kavedaralo magikus tamado alakkent gorkorcsolyak delutani kenyelem legcsevegobb. Borogatasok vasut atkelo legeslegduhosebb Hermesz sajto fahejfai muzeum indoeuropai holdvilag kopenyek kankanok nem beszelek angolul bordas hatvany hetvennegy allami differencialgeometria sargallik melirozas."
+
+helpstring = 'Nyissatok ki az aktataskat. Onnantol mar konnyu lesz.'
+
+stringlong = 'Remaining time: 0 months 0 days 0 hours 43 minutes'
+
+stringshort = 'Project PROMETHEUS'
+
+footerstring = 'www.trap.hu'
+
+lineslist = ['Time until impact:', '0 months 0 days 0 hours 43 minutes']
+
+numberlines = 'abcd efgh ijkl mnop qrst uvwx yz abcd efgh ijkl mnop qrst uvwx yz abcd efgh ijkl mnop qrst uvwx yz abcd efgh ijkl mnop qrst uvwx yz'
+
+
+print_header(stringshort, 'img/darpa-logo.gif', string2=lineslist)
+print_body(helpstring)
+print_footer(footerstring)
+Epson.cut()
